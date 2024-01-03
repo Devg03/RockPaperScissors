e0741f418223dbe5106a872f1b5eb24b990ed8a1
Dev Gandhi <devgandhi914@gmail.com>
2024-01-02 20:41:43 -0500

    The player can enter his selection and the computer generates its own response

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Hello!");
+
+// Computer Choice
+function getComputerChoice() {
+    let randNum = Math.floor(Math.random() * 3);
+
+    if (randNum == 1) {
+        return "Rock";
+    } else if (randNum == 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function getPlayerChoice() {
+    let response = prompt("Please enter your choice:\n1. Rock\n2. Paper\n3. Scissors");
+    return response;
+}
+
+
+const playerSelection = getPlayerChoice()
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
