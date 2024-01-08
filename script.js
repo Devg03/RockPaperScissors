fd3f4e36705de9ad503048c1571983467c337637
Dev Gandhi <devgandhi914@gmail.com>
2024-01-08 01:51:42 -0500

    The game shows an image of the selection/keeps track of player and computer scores

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const paperBtn = document.getElementById('btn2');
 const scissorBtn = document.getElementById('btn3');
 const result = document.getElementById('result');
 
+const playerScore = document.getElementById('player-score');
+const computerScore = document.getElementById('computer-score');
+
+const sign = document.getElementById('sign');
+
+let pScore = 0;
+let cScore = 0;
+
 // Computer Choice
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3);
@@ -19,37 +27,91 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     switch (computerSelection) {
         case "Rock":
-            if (playerSelection == "rock")
+            if (playerSelection == "rock") {
                 return "Replay!!"
-            else if (playerSelection == "paper")
+            }
+            else if (playerSelection == "paper") {
+                pScore = pScore + 1;
+                playerScore.textContent = "Player Score: " + pScore;
                 return `You Win! ${playerSelection} beats ${computerSelection}`;
-            else if (playerSelection == "scissors")
+            }
+            else if (playerSelection == "scissors") {
+                cScore = cScore + 1;
+                computerScore.textContent = "Computer Score: " + cScore;
                 return `You Lose! ${playerSelection} beats ${computerSelection}`;
+            }
         case "Paper":
-            if (playerSelection == "paper")
+            if (playerSelection == "paper") {
                 return "Replay!!"
-            else if (playerSelection == "scissors")
+            }
+            else if (playerSelection == "scissors") {
+                pScore = pScore + 1;
+                playerScore.textContent = "Player Score: " + pScore;
                 return `You Win! ${playerSelection} beats ${computerSelection}`;
-            else if (playerSelection == "rock")
+            }
+            else if (playerSelection == "rock") {
+                cScore = cScore + 1;
+                computerScore.textContent = "Computer Score: " + cScore;
                 return `You Lose! ${playerSelection} beats ${computerSelection}`;
+            }
         case "Scissors":
-            if (playerSelection == "scissors")
+            if (playerSelection == "scissors") {
                 return "Replay!!"
-            else if (playerSelection == "rock")
+            }
+            else if (playerSelection == "rock") {
+                pScore = pScore + 1;
+                playerScore.textContent = "Player Score: " + pScore;
                 return `You Win! ${playerSelection} beats ${computerSelection}`;
-            else if (playerSelection == "paper")
+            }
+            else if (playerSelection == "paper") {
+                cScore = cScore + 1;
+                computerScore.textContent = "Computer Score: " + cScore;
                 return `You Lose! ${playerSelection} beats ${computerSelection}`;
+            }
     }
 }
 
+const rockSign = document.createElement('div');
+const paperSign = document.createElement('div');
+const scissorsSign = document.createElement('div');
+
 rockBtn.addEventListener('click', function () {
+    if (sign.contains(paperSign)) {
+        sign.removeChild(paperSign);
+    } else if (sign.contains(scissorsSign)) {
+        sign.removeChild(scissorsSign);
+    }
+
+    rockSign.setAttribute('style', 'background-image: url("https://i.pinimg.com/736x/ee/99/6d/ee996d592980ec099f24c0836b2b7070.jpg"); visibility: visible;');
+    sign.appendChild(rockSign);
+
     result.textContent = playRound('rock', getComputerChoice());
 });
 
 paperBtn.addEventListener('click', function () {
-    result.textContent = playRound('rock', getComputerChoice());
+    if (sign.contains(rockSign)) {
+        sign.removeChild(rockSign);
+    } else if (sign.contains(scissorsSign)) {
+        sign.removeChild(scissorsSign);
+    }
+
+    paperSign.setAttribute('style', 'background-image: url("https://i.pinimg.com/736x/03/e6/9c/03e69c77ade2b0cf740af358283addd0.jpg"); visibility: visible;');
+    sign.appendChild(paperSign);
+
+    result.textContent = playRound('paper', getComputerChoice());
 });
 
 scissorBtn.addEventListener('click', function () {
-    result.textContent = playRound('rock', getComputerChoice());
+    if (sign.contains(rockSign)) {
+        sign.removeChild(rockSign);
+    } else if (sign.contains(paperSign)) {
+        sign.removeChild(paperSign);
+    }
+
+    scissorsSign.setAttribute('style', 'background-image: url("https://i.pinimg.com/236x/89/ef/5c/89ef5cd84c8a8882dfbafab475e40aac.jpg"); visibility: visible;');
+    sign.appendChild(scissorsSign);
+
+    result.textContent = playRound('scissors', getComputerChoice());
 });
+
+
